2059e4452aa39d2cca3d4f40a1a859b8a4385966
businessmajor <barrios.st@northeastern.edu>
2023-06-26 04:22:34 -0400

    new login screen

--- src/App.js
+++ src/App.js
@@ -14,51 +14,77 @@ import UsersContextLoader from "./users/users-context-loader";
 import ProtectedRoute from "./users/protected-route";
 import { QueryClientProvider } from '@tanstack/react-query';
 import QueryClient from "./services/react-query";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-function App({ wer, ert, rt }) {
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#fd0000',
+    },
+    secondary: {
+      main: '#ffc400',
+    },
+  },
+  shape: {
+    borderRadius: 16,
+  },
+});
+
+function App() {
 
   return (
-    <Provider store={store}>
-      <QueryClientProvider client={QueryClient}>
-        <UsersContextLoader>
-          <BrowserRouter>
-            <div className="container">
-              <Navigation />
-              <Routes>
-                <Route
-                  path="/users/*"
-                  element={
-                    <div className="row">
-                      <div className="col-12">
-                        <Users />
-                      </div>
-                      <div className="col-6">{/* <UserListRedux /> */}</div>
-                    </div>
-                  }
-                />
-                <Route path="/login" element={<LoginScreen />} />
-                <Route path="/register" element={<RegisterScreen />} />
-                <Route
-                  path="/profile"
-                  element={
-                    // <ProtectedRoute>
-                    <ProfileScreen />
-                    /* </ProtectedRoute> */
-                  }
-                />
-                <Route path="/" element={<Navigate to="/login" />} />
-                <Route
-                  path="/driver/*"
-                  element={
-                    <Driver />
-                  }
-                />
-              </Routes>
-            </div>
-          </BrowserRouter>
-        </UsersContextLoader>
-      </QueryClientProvider>
-    </Provider>
+    <React.Suspense
+      fallback={
+        <div className="flex items-center justify-center w-screen h-screen">
+          Loading...
+        </div>
+      }
+    >
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <QueryClientProvider client={QueryClient}>
+            <UsersContextLoader>
+              <BrowserRouter>
+                <div className="container">
+                  <Navigation />
+                  <Routes>
+                    <Route
+                      path="/users/*"
+                      element={
+                        <div className="row">
+                          <div className="col-12">
+                            <Users />
+                          </div>
+                          <div className="col-6">{/* <UserListRedux /> */}</div>
+                        </div>
+                      }
+                    />
+                    <Route path="/login" element={<LoginScreen />} />
+                    <Route path="/register" element={<RegisterScreen />} />
+                    <Route
+                      path="/profile"
+                      element={
+                        // <ProtectedRoute>
+                        <ProfileScreen />
+                        /* </ProtectedRoute> */
+                      }
+                    />
+                    <Route path="/" element={<Navigate to="/driver/explore" />} />
+                    <Route
+                      path="/driver/*"
+                      element={
+                        <Driver />
+                      }
+                    />
+                  </Routes>
+                </div>
+              </BrowserRouter>
+            </UsersContextLoader>
+          </QueryClientProvider>
+        </ThemeProvider>
+      </Provider>
+    </React.Suspense>
   );
 }
 
--- src/users/login.js
+++ src/users/login.js
@@ -2,10 +2,9 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { loginThunk } from "./users-thunks";
 import { useNavigate } from "react-router";
-import { Button, CssBaseline, TextField, Link, Grid, Box, Typography, Container } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Button, CssBaseline, TextField, Link, Grid, Box, Typography, Container, colors } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 
-const defaultTheme = createTheme();
 
 function Copyright(props) {
   return (
@@ -46,69 +45,68 @@ const Login = () => {
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        > <Typography component="h1" variant="h5" className="text-danger fw-bold mb-4">
-            Log in
-          </Typography>
+    // <ThemeProvider >
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      > <Typography component="h1" variant="h5" className="text-danger fw-bold mb-4">
+          Log in
+        </Typography>
 
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              value={user.username}
-              onChange={(e) => setUser({ ...user, username: e.target.value })}
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              name="email"
-              autoComplete="email"
-              autoFocus
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            value={user.username}
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="email"
+            autoComplete="email"
+            autoFocus
 
-            />
-            <TextField
-              margin="normal"
-              value={user.password}
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
+          />
+          <TextField
+            margin="normal"
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
 
-            />
-            <Button
-              type="submit"
-              className="text-danger fw-bold mb-4"
-              onClick={handleLogin}
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
+          />
+          <Button
+            type="submit"
+            onClick={handleLogin}
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="./register" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
             </Grid>
-          </Box>
+          </Grid>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
+    // </ThemeProvider>
 
 
   );
