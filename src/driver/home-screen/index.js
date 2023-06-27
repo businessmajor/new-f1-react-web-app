b7feb407eb7bb5f5727d495978561f0fce81b382
businessmajor <barrios.st@northeastern.edu>
2023-06-27 14:42:50 -0400

    hail mary

--- src/driver/home-screen/index.js
+++ src/driver/home-screen/index.js
@@ -1,18 +1,18 @@
 import React from "react";
-import WhatsHappening from "./whats-happening";
 import { useSelector } from "react-redux";
-// import TuitList from "../tuits/tuits-list";
+import WhatsHappening from "./whats-happening";
 import SpeedList from "./speed-list";
 
 function HomeScreen() {
   const { currentUser } = useSelector((state) => state.users);
+
   return (
     <div>
       <h1 className="text-danger fw-bold" style={{ fontFamily: "Helvetica" }}>Home</h1>
-      {currentUser && (
-        <WhatsHappening />)
-      }
-      <SpeedList/> 
+
+      {currentUser && <WhatsHappening />}
+
+      <SpeedList />
     </div>
   );
 }
