be837577295f8c83d5adae6d38e0dcae9cef954c
fabigazi <fgaziano98@gmail.com>
2023-06-27 01:30:13 -0400

    definatly not going to work

--- src/driver/index.js
+++ src/driver/index.js
@@ -29,6 +29,7 @@ function Driver() {
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="profile" element={<ProfileScreen />} />
+          <Route path="profile/:profileId" element={<ProfileScreen />} />
         </Routes>
       </div>
 
