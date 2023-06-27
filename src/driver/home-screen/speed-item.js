5bc354a4b83dff0d5d39f915daa5ec8e1d2d7c29
businessmajor <barrios.st@northeastern.edu>
2023-06-27 14:52:10 -0400

    pretty tuits

--- src/driver/home-screen/speed-item.js
+++ src/driver/home-screen/speed-item.js
@@ -3,13 +3,13 @@
 import { useDispatch, useSelector } from "react-redux";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-function SpeedItem({speed, username, author, created}) {
+function SpeedItem({ speed, username, author, created }) {
     const { currentUser } = useSelector((state) => state.users);
     let isAdmin = false;
-    if(currentUser) {
-        isAdmin = (currentUser.data.role == "admin");
+    if (currentUser) {
+        isAdmin = (currentUser.data.role === "admin");
     }
-    
+
     const dispatch = useDispatch();
     const deleteTuitHandler = (id) => {
         //dispatch(deleteTuitThunk(id));
@@ -18,26 +18,35 @@ function SpeedItem({speed, username, author, created}) {
     return (
         <li className="list-group-item">
             <div className="row">
-                <div className="col-2">
-                    {//<img width={50} className="float-end rounded-circle" src={`${speed.image}`} />
-                    }
+                <div className="col-auto">
+                    <img
+                        width={50}
+                        className="float-end rounded-circle"
+                        src={require(`./f1.jpg`)}
+                        alt="prof pic"
+                    />
                 </div>
                 <div className="col-10">
-                    {isAdmin &&
-                    <div><i className="bi bi-x float-end"
-                        onClick={() => deleteTuitHandler(speed._id)}></i>
-                    </div>
-                    }
+                    {isAdmin && (
+                        <div>
+                            <i
+                                className="bi bi-x-lg float-end"
+                                onClick={() => deleteTuitHandler(speed._id)}
+                                style={{ cursor: "pointer" }}
+                            ></i>
+                        </div>
+                    )}
                     <div>
-                    <a href={`profile/${author}`}><b>{username}</b></a> {created}
+                        <a href={`profile/${author}`}>
+                            <b>@{username}</b>
+                        </a>{" "}
+                        {created}
                     </div>
-
-
-                    <div>{speed} </div>
-
+                    <div>{speed}</div>
                 </div>
             </div>
         </li>
+
     );
 };
 export default SpeedItem;
